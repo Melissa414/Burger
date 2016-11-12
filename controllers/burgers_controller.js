3c238316ea10958ab7c67e15cc359d06c583074c
Melissa414 <Missy41491@gmail.com>
2016-11-11 23:35:41 -0500

    fixed css & background

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var burgers = require('../models/burgers.js');
+var burger = require('../models/burger.js');
 
 router.get('/', function (req, res) {
   res.redirect('/burgers');
 });
 
 router.get('/burgers', function (req, res) {
-  burgers.all(function (data) {
-    var hbsObject = { burgers: data };
-    console.log(hbsObject);
-    res.render('index', hbsObject);
+  burger.all(function (data) {
+    var burgerObject = { burgers: data };
+    console.log(burgerObject);
+    res.render('index', burgerObject);
   });
 });
 
 router.post('/burgers/create', function (req, res) {
-  burgers.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], function () {
+  burger.create([req.body.newBurgerName], function () {
     res.redirect('/burgers');
   });
 });
 
 router.put('/burgers/update/:id', function (req, res) {
-  var condition = 'id = ' + req.params.id;
+  var condition = req.params.id;
 
   console.log('condition', condition);
 
-  burgers.update({ sleepy: req.body.sleepy }, condition, function () {
+  burgers.update(req.params.id, function () {
     res.redirect('/burgers');
   });
 });
